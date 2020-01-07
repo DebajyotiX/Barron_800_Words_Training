553543bb2bd2b43ea4ecabdfe7b873526ecc139a
Debajyoti Majumder <manofliquidsteel@gmail.com>
2020-01-08 01:23:45 +0530

    Created a funtion which takes the text object and spits out the extracted words and meanings in seperate lists

--- database_reader.py
+++ database_reader.py
@@ -1,31 +1,23 @@
 
-txt_file = open("word_list.txt", "r")
+def reader(txt_file):  #file obj as argument
+	word = []
+	# word.clear()
+	meaning = []
+	# meaning.clear()
 
-word = ["",""]
-word.clear()
-meaning = ["",""]
-meaning.clear()
+	trig = 1 
+	wrd = ""
 
-trig = 1 
-wrd = ""
-
-txt=txt_file.read()
-for c in txt:
-	if c!= "~" and c!= "\n":
-		wrd = wrd + c
-	elif trig == 1 and c== "~":
-		word.append(wrd)
-		wrd = ""
-		trig = - trig
-	elif trig == -1 and c== "~":
-		meaning.append(wrd)
-		wrd = ""
-		trig = - trig
-print("\n".join(word))
-print("\n".join(meaning))
-
-print(len(word))
-print(len(meaning))
-
-txt_file.close() 
-
+	txt=txt_file.read()
+	for c in txt:
+		if c!= "~" and c!= "\n":
+			wrd = wrd + c
+		elif trig == 1 and c== "~":
+			word.append(wrd)
+			wrd = ""
+			trig = - trig
+		elif trig == -1 and c== "~":
+			meaning.append(wrd)
+			wrd = ""
+			trig = - trig
+	return word,meaning
