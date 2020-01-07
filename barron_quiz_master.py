ec9dcaf4956b552d8a15084fcba802d9cce9bdcd
Debajyoti Majumder <manofliquidsteel@gmail.com>
2020-01-08 01:27:07 +0530

    Calls the function reader to get the extracted data

--- barron_quiz_master.py
+++ barron_quiz_master.py
@@ -1,3 +1,18 @@
 import numpy as np
 import tkinter
-print("ehskdfn")
+from database_reader import reader
+# from permutator_gui import permutator
+
+txt_file = open("word_list.txt", "r")
+word,meaning =reader(txt_file) #file obj as argument
+txt_file.close() 
+# print(word,end="\n")
+# print(meaning,end="\n")
+
+# print(len(word))
+# print(len(meaning))
+print("Enter Recap Style")
+print("Meaning to Word; enter:'['")
+choice = raw_input("Word to Meaning; enter:']' \n ") 
+if choice =="[":
+	
